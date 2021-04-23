79e3530916406698a6b142522f75ef117afda313
AbanobWadie <abanob.wadi3@gmail.com>
2021-04-23 15:51:53 +0200

    v 2

--- SportsApp/URLs.swift
+++ SportsApp/URLs.swift
@@ -10,7 +10,9 @@ import Foundation
 class URLs {
     private static let baseUrl = "https://www.thesportsdb.com/api/v1/json/1/"
     
+    static let getSports = baseUrl + "all_sports.php"
     static let getLeagues = baseUrl + "all_leagues.php"
     static let getLeagueDetailsById = baseUrl + "lookupleague.php?id="
+    
 }
 
